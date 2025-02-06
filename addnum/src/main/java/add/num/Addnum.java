f42aa924e835e5c4071242d697daec242233bd48
D E L L <D E L L@DESKTOP-6L02Q21>
2025-02-06 23:43:01 +0500

    sample upload

---
+++ addnum/src/main/java/add/num/Addnum.java
@@ -0,0 +1,16 @@
+package add.num;
+
+public class Addnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=7;
+		int b=9;
+		int c=a+b;
+		System.out.println("addition:"+c); 
+		
+				
+}
+
+}
